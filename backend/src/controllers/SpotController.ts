09252cebebc41a8fefa8542e97cd41bb5991a158
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-12-19 14:14:31 -0300

    feat: (Spot) add Spot in SpotController

---
+++ backend/src/controllers/SpotController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+import Spot from "../models/Spot";
+
+export class SpotController {
+  static async store(req: Request, res: Response): Promise<any> {
+    const { filename }: any = req.file;
+
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: "User does not exists" });
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(",").map((tech: string) => tech.trim()),
+      price,
+    });
+    return res.json(spot);
+  }
+}
