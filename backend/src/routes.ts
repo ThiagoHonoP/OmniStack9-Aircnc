43af8dfbcd1c23686f108b16761ea4beee8fc9bf
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-12-22 19:45:43 -0300

    feat: add route booking

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,11 +3,18 @@ import { SessionController } from "./controllers/SessionController";
 import { SpotController } from "./controllers/SpotController";
 import multer from "multer";
 import { storage } from "./config/upload";
+import { DashboardController } from "./controllers/DashboardController";
+import { BookingController } from "./controllers/BookingController";
 
 const routes = Router();
 const upload = multer({ storage });
 
 routes.post("/sessions", SessionController.store);
+routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+
+routes.get("/dashboard", DashboardController.index);
+
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 export default routes;
