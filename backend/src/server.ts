5cbff4736a317304719f6afd3bb484ae236a0310
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2025-01-26 17:21:41 -0300

    feat: add cors() package in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import { connectDB } from "./config/database";
 import SessionRouter from "./routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use();
 connectDB();
 app.use(express.json());
 
 app.use(SessionRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
